a24d13d2121f18a6e00da082b31298b9c00fba6d
yohagos <yosef.hagos@googlemail.com>
2024-06-27 16:45:20 +0200

    feat: created / modified docker-compose for postgres and keycloak, gitignore, requirements for necessary libraries and main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import logging
+from fastapi import FastAPI
+
+from routes.router import configureRouter
+from database.db import engine
+
+from schemas.characters import Base as characters_base
+from schemas.techniques import Base as techniques_base
+from schemas.villages import Base as villages_base
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+configureRouter(app)
+
+try:
+    characters_base.metadata.create_all(engine)
+    techniques_base.metadata.create_all(engine)
+    villages_base.metadata.create_all(engine)
+    logger.info("Tables created successfully.")
+except Exception as e:
+    logger.error("Error creating tables: %s", e)
